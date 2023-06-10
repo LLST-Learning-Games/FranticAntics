e321b50adfe761c46d766fedfdec7a9a0cb3a08d
Brandon Dahn <brandond.mobile@gmail.com>
2023-06-10 15:32:56 -0700

    Setup left stick for legacy input

--- Assets/Scripts/Queen/Queen.cs
+++ Assets/Scripts/Queen/Queen.cs
@@ -9,8 +9,6 @@ namespace  AntQueen
 {
     public class Queen : MonoBehaviour
     {
-
-        [SerializeField] private InputReader _queenInput;
         [SerializeField] private Transform _targetObject;
         [SerializeField] private float _targetDistance;
         [SerializeField] private float _speed;
@@ -21,28 +19,38 @@ namespace  AntQueen
         
         void Start()
         {
-            _queenInput.spawnAnt += OnSpawnAnt;
+            //_queenInput.spawnAnt += OnSpawnAnt;
         }
 
         void Update()
         {
-            HandleMovement();
+            Debug.Log("1: " + GetController("Left", 0));
+            Debug.Log("2: " + GetController("Left", 1));
+            //HandleMovement();
         }
 
         private void HandleMovement()
         { 
-            Vector3 movement = new Vector3(_queenInput.movement.x, 0, _queenInput.movement.y);
-            transform.Translate(movement * Time.deltaTime * _speed);
-            //transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movement), Time.deltaTime * 40f);
-            _animator.speed = movement.magnitude * _speed;
+            //Vector3 movement = new Vector3(_queenInput.movement.x, 0, _queenInput.movement.y);
+            //transform.Translate(movement * Time.deltaTime * _speed);
+            ////transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movement), Time.deltaTime * 40f);
+            //_animator.speed = movement.magnitude * _speed;
             
-            Vector3 target = new Vector3(_queenInput.target.x, 0, _queenInput.target.y);
-            _targetObject.transform.SetLocalPositionAndRotation(target * _targetDistance, Quaternion.identity);
+            //Vector3 target = new Vector3(_queenInput.target.x, 0, _queenInput.target.y);
+            //_targetObject.transform.SetLocalPositionAndRotation(target * _targetDistance, Quaternion.identity);
         }
 
         public void OnSpawnAnt()
         {
             Instantiate(_antPrefab,_targetObject.transform.position,Quaternion.identity);
+        }
+
+        private Vector2 GetController(string stick, int controller)
+        {
+            return new Vector2(
+                Input.GetAxis($"Horizontal-{stick}-{controller}"),
+                Input.GetAxis($"Vertical-{stick}-{controller}")
+            );
         }
     }   
 }
