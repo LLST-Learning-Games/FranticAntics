7fc6b5c5dd9872ca6e5da02a116ccf485f74d0b5
Matthew Stevens <matthew@rogueharbour.com>
2023-06-10 15:42:25 -0700

    2 players queen movement

--- Assets/Scripts/Queen/Queen.cs
+++ Assets/Scripts/Queen/Queen.cs
@@ -9,6 +9,7 @@ namespace  AntQueen
 {
     public class Queen : MonoBehaviour
     {
+        [SerializeField] private int _playerNumber;
         [SerializeField] private Transform _targetObject;
         [SerializeField] private float _targetDistance;
         [SerializeField] private float _speed;
@@ -22,14 +23,30 @@ namespace  AntQueen
             //_queenInput.spawnAnt += OnSpawnAnt;
         }
 
-        void Update()
+        void Update() 
         {
-            Debug.Log("1: " + GetController("Left", 0));
-            Debug.Log("2: " + GetController("Left", 1));
-            //HandleMovement();
+        //     Debug.Log("1: " + GetController("Left", 0));
+        //     Debug.Log("2: " + GetController("Left", 1));
+            HandleMovement();
         }
 
         private void HandleMovement()
+        {
+            var leftInput = GetController("Left", _playerNumber);
+            Vector3 movement = new Vector3(leftInput.x, 0, leftInput.y);
+            transform.Translate(movement * Time.deltaTime * _speed);
+            //transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movement), Time.deltaTime * 40f);
+            _animator.speed = movement.magnitude * _speed;
+        }
+
+        private void HandleTarget()
+        {
+            var rightInput = GetController("Right", _playerNumber);
+            Vector3 target = new Vector3(rightInput.x, 0, rightInput.y);
+            _targetObject.transform.SetLocalPositionAndRotation(target * _targetDistance, Quaternion.identity);
+        }
+
+        private void HandleMovementNewInput()
         { 
             //Vector3 movement = new Vector3(_queenInput.movement.x, 0, _queenInput.movement.y);
             //transform.Translate(movement * Time.deltaTime * _speed);
