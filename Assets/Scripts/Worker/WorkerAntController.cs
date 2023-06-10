ddfe7f07b38bf3b61143872362e2ca12259e1a43
RogueUmut <umutcan@rogueharbour.com>
2023-06-10 13:06:56 -0700

    WorkerAntManager and workerAnt status

--- Assets/Scripts/Worker/WorkerAntController.cs
+++ Assets/Scripts/Worker/WorkerAntController.cs
@@ -1,89 +1,43 @@
 using System;
 using UnityEngine;
-using UnityEngine.AI;
-using Random = UnityEngine.Random;
 
 namespace Worker
 {
     public class WorkerAntController : MonoBehaviour
     {
-        [SerializeField] protected Animator _animator;
+        [SerializeField] protected WorkerAntMovement _antMovement;
 
-        [Header("AI")]
-        [SerializeField] protected Transform _navMeshTarget;
-        [SerializeField] protected NavMeshAgent _navMeshAgent;
-
-        private bool _isStopped;
-        private bool _isIdle;
+        public WorkerAntStatus Status;
+        
+        private bool _isStopped => _antMovement.IsStopped;
+        private bool _isIdle => _antMovement.IsIdle;
 
         public Action<WorkerAntController> OnPathStarted;
         public Action<WorkerAntController> OnPathCompleted;
 
         private void Start()
         {
+            _antMovement.Initialize(this);
+            
+            WorkerAntManager.Instance.RegisterWorkerAnt(this);
+        }
+
+        private void OnDestroy()
+        {
+            if(WorkerAntManager.Instance != null)
+                WorkerAntManager.Instance.RemoveWorkerAnt(this);
         }
 
         private void Update()
         {
-            SetDestination(null);
-
-            ProcessAntMovement();
+            _antMovement.ProcessAntMovement();
         }
 
-        private void ProcessAntMovement()
+        public bool SetDestination(Vector3 destination, Action<WorkerAntController> onPathCompleted)
         {
-            if(_navMeshAgent.path == null)
-                return;
-
-            // if the ant stopped play idle animation
-            if (_navMeshAgent.velocity == Vector3.zero && !_isIdle)
-            {
-                AntPathStarted();
-                return;
-            }
-
-            if (_navMeshAgent.velocity != Vector3.zero && _isStopped)
-            {
-                AntPathCompleted();
-            }
-        }
-
-        private void StartRandomIdleAnimation()
-        {
-            _animator.SetTrigger("idle_" + Random.Range(0,3));
-            _isIdle = true;
-            _isStopped = true;
-        }
-
-        private void PlayMovementAnimation()
-        {
-            _animator.SetTrigger("walk");
-            _isIdle = false;
-            _isStopped = false;
-        }
-
-        public bool SetDestination(Action<WorkerAntController> onPathCompleted)
-        {
-            var result = _navMeshAgent.SetDestination(_navMeshTarget.position);
             OnPathCompleted += onPathCompleted;
             
-            return result;
-        }
-
-        public virtual void AntPathStarted()
-        {
-            Debug.Log("[WorkerAntController] Ant stopped");
-            StartRandomIdleAnimation();
-            
-            OnPathStarted?.Invoke(this);
-        }
-
-        public virtual void AntPathCompleted()
-        {
-            Debug.Log("[WorkerAntController] Ant started moving");
-            PlayMovementAnimation();
-            
-            OnPathCompleted?.Invoke(this);
+            return _antMovement.SetDestination(destination);
         }
     }
 }
