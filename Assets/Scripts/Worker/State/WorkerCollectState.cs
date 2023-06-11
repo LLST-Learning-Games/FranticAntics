99aec6001c9e758ecd2b58527c2610067474e7da
RogueUmut <umutcan@rogueharbour.com>
2023-06-10 18:54:01 -0700

    minor bug fixed

--- Assets/Scripts/Worker/State/WorkerCollectState.cs
+++ Assets/Scripts/Worker/State/WorkerCollectState.cs
@@ -51,39 +51,46 @@ namespace Worker.State
                 return;
             
             base.UpdateState();
-            
-            if(TargetCollectable.ItemCollected && !_itemCollected)
-                _workerAntController.Whistle(Vector3.zero);
 
-            if (!_itemCollected && Vector3.Distance(TargetCollectable.transform.position, transform.position) < .5f)
+            try
             {
-                _itemCollected = true;
-                
-                if(!TargetCollectable.Mineable)
-                    _collectedPiece = TargetCollectable.gameObject;
-                else
-                    _collectedPiece = TargetCollectable.Mine();
-                
-                _collectedPiece.transform.SetParent(_carryParent);
-                _collectedPiece.transform.DOLocalMove(Vector3.zero, .2f).OnComplete(() =>
-                {
-                    if(!TargetCollectable.Mineable)
-                        TargetCollectable.ItemCollected = true;
-                });
-            }
-            else if (_itemCollected && _collectedPiece != null)
-            {
-                _workerAntController.SetDestination(_endDestination.position);
-
-                if (Vector3.Distance(transform.position, _endDestination.position) < .5f)
-                {
-                    _collectedPiece.transform.SetParent(null);
-                    _collectedPiece.transform.DOScale(Vector3.zero, .2f);
-                    _collectedPiece.transform.DOMove(_endDestination.position, .2f);
-                    TargetCollectable.Consume(_workerAntController.TeamController);
-
+                if(TargetCollectable.ItemCollected && !_itemCollected)
                     _workerAntController.Whistle(Vector3.zero);
+
+                if (!_itemCollected && Vector3.Distance(TargetCollectable.transform.position, transform.position) < .5f)
+                {
+                    _itemCollected = true;
+                
+                    if(!TargetCollectable.Mineable)
+                        _collectedPiece = TargetCollectable.gameObject;
+                    else
+                        _collectedPiece = TargetCollectable.Mine();
+                
+                    _collectedPiece.transform.SetParent(_carryParent);
+                    _collectedPiece.transform.DOLocalMove(Vector3.zero, .2f).OnComplete(() =>
+                    {
+                        if(!TargetCollectable.Mineable)
+                            TargetCollectable.ItemCollected = true;
+                    });
                 }
+                else if (_itemCollected && _collectedPiece != null)
+                {
+                    _workerAntController.SetDestination(_endDestination.position);
+
+                    if (Vector3.Distance(transform.position, _endDestination.position) < .5f)
+                    {
+                        _collectedPiece.transform.SetParent(null);
+                        _collectedPiece.transform.DOScale(Vector3.zero, .2f);
+                        _collectedPiece.transform.DOMove(_endDestination.position, .2f);
+                        TargetCollectable.Consume(_workerAntController.TeamController);
+
+                        _workerAntController.Whistle(Vector3.zero);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _workerAntController.Whistle(Vector3.zero);
             }
         }
     }
